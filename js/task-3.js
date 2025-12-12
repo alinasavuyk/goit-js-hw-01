fd32fc7e57ed5dacfe5efc50c3bd77c52989f025
alinasavuyk <alinabondarenko>
2025-12-12 15:56:12 +0200

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
-function getElementWidth(content, padding, border) {
-    const boxSizing = Number.parseFloat(content) + Number.parseFloat(padding)*2 + Number.parseFloat(border)*2;
+function getElementWidth(content, padding, border) { 
+const boxSizing = Number.parseFloat(content) + Number.parseFloat(padding)*2 + Number.parseFloat(border)*2;
     return boxSizing;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
